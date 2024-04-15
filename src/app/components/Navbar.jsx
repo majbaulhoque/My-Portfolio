6dcdc4b1fcbcc627f4c7fe5e5dd0e4c73970429f
majbaulhoque <majba9399@gmail.com>
2024-04-15 23:40:51 +0600

    added three dot and corss bar in small device

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import React, {useState} from 'react';
 import NavLink from './NavLink';
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const navLinks = [
     {
@@ -23,10 +24,19 @@ const Navbar = () => {
     return (
         <nav className='fixed
         top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
-            <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
+            <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 {/* <Link href={"/"} className='text-5xl text-white font-semibold'>LOGO</Link> */}
                 <a href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</a>
-                <div className='menu md:block md:w-auto' id='navbar'>
+                <div className='mobile-menu block md:hidden'>
+                {
+                    !navbarOpen ? (
+                        <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py- border rounded border-slate-200 hover:text-white hover:border-white text-slate-200'><Bars3Icon className='h-5 w-5'/></button>
+                    ) : (
+                        <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py- border rounded border-slate-200 hover:text-white hover:border-white text-slate-200'><XMarkIcon className='h-5 w-5'/></button>
+                    )
+                }
+                </div>
+                <div className='menu block md:block md:w-auto' id='navbar'>
                     <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
                         {
                             navLinks.map((link, idx) => (<li key={idx}>
